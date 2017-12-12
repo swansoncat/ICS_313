200004f03422f383935b6562b2dd76f6a82f43da
swansoncat <swansoncat@users.noreply.github.com>
2017-12-11 16:55:26 -1000

    more things implemented

--- knbagano_a7.pl
+++ knbagano_a7.pl
@@ -3,11 +3,13 @@ use feature 'say';
 
 say "Is this file a person or movie?";
 my $category = <STDIN>;
+chomp($category);
 
-while ($category ne "person\n" && category ne "movie\n")
+while ($category ne "person" && category ne "movie")
 {
 	say "That isn't a valid category. Please enter either \'person\' or \'movie\'.";
 	$category = <STDIN>;
+	chomp($category);
 }
 
 
@@ -15,18 +17,47 @@ while ($category ne "person\n" && category ne "movie\n")
 my $file_name = 'jj_abrams.html';
 my $write_file = 'database.pl';
 
+my $title;
+
 open my $fh, '<', $file_name or die "Can't open read file: $_";
 open my $fh_w, '>>', $write_file or die "Can't open write file: $_";
 
 while (my $info = <$fh>) 
 {
 	chomp($info);
-	if ($info =~ m/nm_flmg_dr/)
+	if ($info =~ m/<title>/)
 	{
-		print "$info\n";
-		print $fh_w "$info\n";
+		my $title_index = index $info, ">";
+		my $title_end = index $info, "IDMB";
+		$title = substr $info, $title_index + 1, $title_end - $title_index;
 	}
+	
+	if ($info =~ m/nm_flmg_dr/ && $info =~ m/<b>/)
+	{
+		my $length = length $info;		
+		my $substr = substr $info, 40, $length - 40;
+		my $start_index = index $substr, ">";
+		my $end_index = index $substr, "<";
+		$substr = substr $substr, $start_index + 1, $end_index - $start_index - 1;
+		print "$substr\n";
+		print $fh_w "directed($title, $substr)\n";
+	}
+	
+	if ($info =~ m/nm_flmg_act/ && $info =~ m/<b>/)
+	{
+		my $length = length $info;		
+		my $substr = substr $info, 40, $length - 40;
+		my $start_index = index $substr, ">";
+		my $end_index = index $substr, "<";
+		$substr = substr $substr, $start_index + 1, $end_index - $start_index - 1;
+		print "$substr\n";
+		print $fh_w "acts_in($title, $substr)\n";
+	}
+	
 }
 
 close $fh or die "Unable to close: $_";
 close $fh_w or die "Unable to close write file: $_";
+
+
+
